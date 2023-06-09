1a31fb015834e9972af47573a1cde08a52b45d29
KelvinYu223 <yhanyuan@gmail.com>
2023-06-09 21:34:07 +0800

    first commit for initiate part of the project until the text recognition part (excluded copy and paste function)

---
+++ app/src/main/java/com/example/mytextrecognition/MainActivity.java
@@ -0,0 +1,126 @@
+package com.example.mytextrecognition;
+
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.Point;
+import android.graphics.Rect;
+import android.net.Uri;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.github.dhaval2404.imagepicker.ImagePicker;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.ml.vision.FirebaseVision;
+import com.google.firebase.ml.vision.common.FirebaseVisionImage;
+import com.google.firebase.ml.vision.text.FirebaseVisionText;
+import com.google.firebase.ml.vision.text.FirebaseVisionTextRecognizer;
+import com.google.mlkit.vision.common.InputImage;
+import com.google.mlkit.vision.text.Text;
+import com.google.mlkit.vision.text.TextRecognition;
+import com.google.mlkit.vision.text.TextRecognizer;
+import com.google.mlkit.vision.text.latin.TextRecognizerOptions;
+
+import java.io.IOException;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    ImageView imageView;
+    TextView detectedText;
+    Button btn_detect,pick_image;
+    Bitmap imageBitmap;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main_activity);
+
+        imageView = findViewById(R.id.img);
+        detectedText = findViewById(R.id.detectedText);
+        btn_detect = findViewById(R.id.button_detect);
+        pick_image = findViewById(R.id.pickImage);
+        pick_image.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ImagePicker.with(MainActivity.this)
+                        .crop()
+                        .compress(1024)
+                        .maxResultSize(1080, 4000)
+                        .start();
+            }
+        });
+        btn_detect.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                detect();
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+            Uri imageUri = data.getData();
+        try {
+            imageBitmap = MediaStore.Images.Media.getBitmap(this.getContentResolver(), imageUri);
+            imageView.setImageBitmap(imageBitmap);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void detect() {
+//        FirebaseVisionImage image = FirebaseVisionImage.fromBitmap(imageBitmap);
+//        FirebaseVisionTextRecognizer detector = FirebaseVision.getInstance().getCloudTextRecognizer();
+//        detector.processImage(image).addOnSuccessListener(firebaseVisionText -> processTxt(firebaseVisionText)).addOnFailureListener(new OnFailureListener() {
+//            @Override
+//            public void onFailure(@NonNull Exception e) {
+//                Toast.makeText(MainActivity.this, "Fail to detect the text from image..", Toast.LENGTH_SHORT).show();
+//            }
+//        });
+        InputImage image = InputImage.fromBitmap(imageBitmap, 0);
+        TextRecognizer recognizer = TextRecognition.getClient(TextRecognizerOptions.DEFAULT_OPTIONS);
+        Task<Text> result =
+                recognizer.process(image)
+                        .addOnSuccessListener(new OnSuccessListener<Text>() {
+                            @Override
+                            public void onSuccess(Text visionText) {
+                                Toast.makeText(MainActivity.this, "Text detected successful....", Toast.LENGTH_SHORT).show();
+                                detectedText.setText(visionText.getText());
+                            }
+                        })
+                        .addOnFailureListener(
+                                new OnFailureListener() {
+                                    @Override
+                                    public void onFailure(@NonNull Exception e) {
+                                        Toast.makeText(MainActivity.this, "Failed to detected text.....", Toast.LENGTH_SHORT).show();
+                                    }
+                                });
+
+
+    }
+
+//    public void processTxt(FirebaseVisionText text) {
+//        List<FirebaseVisionText.TextBlock> blocks = text.getTextBlocks();
+//        if (blocks.size() == 0) {
+//            Toast.makeText(MainActivity.this, "No Text ", Toast.LENGTH_LONG).show();
+//            return;
+//        }
+//
+//        for (FirebaseVisionText.TextBlock block : text.getTextBlocks()) {
+//            String txt = block.getText();
+//            detectedText.setText(txt);
+//        }
+//    }
+}
