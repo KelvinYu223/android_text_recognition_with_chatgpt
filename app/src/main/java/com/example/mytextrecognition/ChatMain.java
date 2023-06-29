8ce6f9f47c59b8ab0afaa345bc6a3062c2d5e101
KelvinYu223 <yhanyuan@gmail.com>
2023-06-29 23:03:28 +0800

    added menu item in chat menu.

--- app/src/main/java/com/example/mytextrecognition/ChatMain.java
+++ app/src/main/java/com/example/mytextrecognition/ChatMain.java
@@ -2,27 +2,46 @@ package com.example.mytextrecognition;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class ChatMain extends AppCompatActivity {
 
-    Button mock;
+    Button detectButton;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.chat_main);
-        mock = findViewById(R.id.mockbutton);
-        mock.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(ChatMain.this, MainActivity.class);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.main_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle item selection
+        switch (item.getItemId()) {
+            case R.id.button_detect:
+                Intent intent = new Intent(this, MainActivity.class);
                 startActivity(intent);
-            }
-        });
+                return true;
+            case R.id.disclaimer:
+                Toast.makeText(this, "Disclaimer", Toast.LENGTH_SHORT).show();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 }
