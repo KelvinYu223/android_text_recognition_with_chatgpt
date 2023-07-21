050c84f0b4b5d7a43146a80f25de59d1c22df054
KelvinYu223 <yhanyuan@gmail.com>
2023-07-21 16:36:59 +0800

    using pending flag for devices with android 12 and above

--- app/src/main/java/com/example/mytextrecognition/Text_Detect_Widget.java
+++ app/src/main/java/com/example/mytextrecognition/Text_Detect_Widget.java
@@ -5,14 +5,18 @@ import android.appwidget.AppWidgetManager;
 import android.appwidget.AppWidgetProvider;
 import android.content.Context;
 import android.content.Intent;
+import android.os.Build;
 import android.widget.RemoteViews;
 
+import androidx.annotation.RequiresApi;
+
 public class Text_Detect_Widget extends AppWidgetProvider {
+    @RequiresApi(api = Build.VERSION_CODES.M)
     @Override
     public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
         for (int appWidgetId : appWidgetIds) {
             Intent intent = new Intent(context, MainActivity.class);
-            PendingIntent pendingIntent = PendingIntent.getActivity(context, 0, intent, 0);
+            PendingIntent pendingIntent = PendingIntent.getActivity(context, 0, intent, PendingIntent.FLAG_IMMUTABLE);
 
             RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.text_detect_widget);
             views.setOnClickPendingIntent(R.id.text_detect_button, pendingIntent);
